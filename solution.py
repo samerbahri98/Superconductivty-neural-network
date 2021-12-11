8aa5b8d216f942cf397cbcceb3ef7fac5a30ce48
samer.bahri98 <samer.bahri@ieee.org>
2021-12-11 12:44:33 +0100

    feat: forward propagation

--- solution.py
+++ solution.py
@@ -1,18 +1,92 @@
-for _ in range(17011):
-    input()
+from random import uniform
 
+train_input_array = []
+train_output_array = []
+test_input_array = []
+maxes = [0 for _ in range(81)]
+mins = [0 for _ in range(81)]
+
+
+### DATA INPUT ###
 for _ in range(17011):
-    input()
+    line = input()
+    line = line.split("\t")
+    fields = []
+    for i in range(len(line)):
+        value = float(line[i])
+        fields.append[value]
+        if(maxes[i] < value):
+            maxes[i] = value
+        if(mins[i] > 0):
+            mins[i] = value
+
+    train_input_array.append(fields)
+
+for i in range(17011):
+    line = input()
+    train_output_array.append(float(line))
+    for j in range(len(train_input_array[i])):
+        train_input_array[i][j] = (train_input_array[i][j]-mins[j])/maxes[j]
+
 
 for _ in range(4253):
-    input()
+    line = input()
+    line = line.split("\t")
+    fields = []
+    for i in range(len(line)):
+        value = float(line[i])
+        fields.append[(value-mins[i])/maxes[j]]
+    test_input_array.append(fields)
+
+
+# Classes
+class Matrix:
+    def __init__(self, data):
+        self.data = data
+
+    def multiply(self, b):
+        result = []
+        for i in range(len(self.data)):
+            for j in range(len(b.data[0])):
+                for k in range(len(b)):
+                    result[i][j] += self.data[i][k] * b.data[k][j]
+        return Matrix(result)
+
+    # Leaky Relu
+    def activate(self, beta=0.1):
+        current_data = self.data
+        for i in range(len(current_data)):
+            for j in range(len(current_data[0])):
+                if current_data[i][j] <= 0:
+                    current_data[i][j] *= beta
+        return Matrix(current_data)
+
+
+class Layer:
+    def __init__(self, number_of_neurons, previous_layer) -> None:
+        self.number_of_neurons: Matrix = number_of_neurons
+        self.previous_layer: Matrix = previous_layer
+        weights = []
+        for i in range(len(maxes)):
+            for j in range(number_of_neurons):
+                weights[i, j] = uniform(0, 5000)
+        self.weights = Matrix(weights)
+
+    def forward(self):
+        multiplication: Matrix = self.previous_layer.multiply(self.weights)
+        self.current_layer = multiplication.activate()
+        return self.current_layer
+
+
+# PREPARE NETWORK
+
+
+train_input = Matrix(train_input_array)
+train_output = Matrix(train_output_array)
+test_input = Matrix(test_input_array)
 
 result = []
 
-for i in range(4253):
-    x = input()
-    result.append(x)
-    
 for i in result:
-    if(i!=""):
+    if(i != ""):
         print(i)
--- test.py
+++ test.py
@@ -3,24 +3,28 @@ from random import shuffle
 import math
 import subprocess
 
-def rmse (y,yhat):
+
+def rmse(y, yhat):
     sum_result = 0
     for i in range(len(y)):
-        sum_result+=(float(y[i])-float(yhat[i]))*(float(y[i])-float(yhat[i]))
+        sum_result += (float(y[i])-float(yhat[i]))*(float(y[i])-float(yhat[i]))
     return math.sqrt(sum_result/len(y))
 
-datasetFile = open("./dataset/train.csv","r")
 
-datasetLines=datasetFile.read().split("\n")
+datasetFile = open("./dataset/train.csv", "r")
+
+datasetLines = datasetFile.read().split("\n")
 datasetFile.close()
-shuffle(datasetLines)
+shuffle(datasetLines[1:len(datasetLines)])
 
-commaSeparateLine = lambda dataset : [line.split(",") for line in dataset]
 
-trainSet = commaSeparateLine(datasetLines[0:17011])
-testSet = commaSeparateLine(datasetLines[17011:len(datasetLines)])
+def comma_separate_line(dataset): return [line.split(",") for line in dataset]
 
-input =""
+
+trainSet = comma_separate_line(datasetLines[0:17011])
+testSet = comma_separate_line(datasetLines[17011:len(datasetLines)])
+
+input = ""
 
 
 input += "\n".join(["\t".join(line[0:81]) for line in trainSet])+"\n"
@@ -28,15 +32,13 @@ input += "\n".join([line[81] for line in trainSet])+"\n"
 input += "\n".join(["\t".join(line[0:81]) for line in testSet])+"\n"
 input += "\n".join([line[81] for line in testSet])
 
-p = subprocess.Popen(["python","./solution.py"],stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
+p = subprocess.Popen(["python", "./solution.py"], stdout=subprocess.PIPE,
+                     stdin=subprocess.PIPE, stderr=subprocess.PIPE)
 
 solutionOutput = p.communicate(input=input.encode())[0].decode()
 
 predictions = solutionOutput.split("\n")[0:-1]
 
-print(rmse(predictions,[line[81] for line in testSet]))
+print(predictions)
 
-# print(predictions,[line[81] for line in testSet])
-
-
-# print (input)
+# print(rmse(predictions,[line[81] for line in testSet]))
